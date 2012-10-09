b7f04f72206afe618cdb5594ba85a40fe931aef2
Ahmed Ezzat Afifi <AEzzatA@gmail.com>
2012-10-09 21:37:00 +0200

    file has been added

---
+++ bSearch.py
@@ -0,0 +1,17 @@
+import sys
+
+def bSearch(Phrase, SortedList):
+	if (Phrase < SortedList[0]) or (Phrase > SortedList[len(SortedList)-1]):
+		return "Item isn't in the givin list"
+		#sys.exit(0)
+	if (len(SortedList) == 2):
+		if (Phrase == SortedList[0]):
+			return SortedList[0]
+			#sys.exit()
+		else: 
+			return SortedList[1]
+			#sys.exit(0)
+	if(Phrase < (len(SortedList)/2)):
+		bSearch(Phrase,SortedList[0:len(SortedList)/2 -1])
+	else:
+		bSearch(Phrase, SortedList[len(SortedList)/2 : len(SortedList)-1])
