ff292307d3d07799acd270f1f20c518b7a20406a
Ahmed Ezzat Afifi <AEzzatA@gmail.com>
2012-10-09 21:40:41 +0200

    Removed import sys

--- bSearch.py
+++ bSearch.py
@@ -1,9 +1,7 @@
-import sys
-
 def bSearch(Phrase, SortedList):
 	if (Phrase < SortedList[0]) or (Phrase > SortedList[len(SortedList)-1]):
 		return "Item isn't in the givin list"
-		#sys.exit(0)
+		#sys.exit(0) #Import sys and un-comment these lines if you want!
 	if (len(SortedList) == 2):
 		if (Phrase == SortedList[0]):
 			return SortedList[0]
